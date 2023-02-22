ec3517126a1485925df5f784142ab7c99ee389ca
piggy <2414343378@qq.com>
2023-02-22 20:30:01 +0800

    maven

--- springboot-vue-demo/vue/src/views/Home.vue
+++ springboot-vue-demo/vue/src/views/Home.vue
@@ -3,15 +3,15 @@
 
 <!--    功能区域-->
     <div style="margin:10px 0">
-      <el-button type="primary">新增</el-button>
+      <el-button type="primary"  @click="add">新增</el-button>
       <el-button type="primary">导入</el-button>
       <el-button type="primary">导出</el-button>
     </div>
 
 <!--    搜索区域-->
     <div style="margin:10px 0">
-      <el-input v-model="search" placeholder="Please input key word" style="width:20%"/>
-      <el-button type="primary" style="margin-left:5px">查询</el-button>
+      <el-input v-model="search" placeholder="Please input key word" style="width:20%" clearable/>
+      <el-button type="primary" style="margin-left:5px" @click="load">查询</el-button>
     </div>
 
 
@@ -19,19 +19,27 @@
         :data="tableData"
         border
         stripe
-        style="width: 100%">
+        style="width: 100vh">
       <el-table-column
-          prop="date"
-          label=”date“
-          sortable
-      />
+          prop="id"
+          label=”ID“
+          sortable />
       <el-table-column
-          prop="name"
-          label="Name"  />
+          prop="username"
+          label="用户名"  />
+      <el-table-column
+          prop="nickName"
+          label="昵称" />
+      <el-table-column
+          prop="age"
+          label="年龄"  />
+      <el-table-column
+          prop="sex"
+          label="性别" />
       <el-table-column
           prop="address"
-          label="Address" />
-      <el-table-column fixed="right" label="Operations" width="120">
+          label="地址"  />
+      <el-table-column fixed="right" label="操作" width="120">
         <template #default>
           <el-button link type="primary" size="small" @click="handleEdit(scope.row)">编辑</el-button>
           <el-popconfirm title="确认删除吗?">
@@ -43,17 +51,56 @@
       </el-table-column>
     </el-table>
 
+    <div>
+      <el-dialog
+          v-model="dialogVisible"
+          title="Tips"
+          width="30%">
+        <el-form :model="form" label-width="120px">
+          <el-form-item label="用户名">
+            <el-input v-model="form.username" style = "width:80%"/>
+          </el-form-item>
+          <el-form-item label="昵称">
+            <el-input v-model="form.nickName" style = "width:80%"/>
+          </el-form-item>
+          <el-form-item label="年龄">
+            <el-input v-model="form.age" style = "width:80%"/>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-radio v-model="form.sex" label="男" >男</el-radio>
+            <el-radio v-model="form.sex" label="女" >女</el-radio>
+            <el-radio v-model="form.sex" label="未知" >未知</el-radio>
+          </el-form-item>
+          <el-form-item label="地址">
+            <el-input type = "textarea" v-model="form.address" style = "width:80%"/>
+          </el-form-item>
+        </el-form>
+
+        <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="save">
+          确定
+        </el-button>
+      </span>
+        </template>
+      </el-dialog>
+
+
+    </div>
+
 <!--    分页-->
     <div style="margin:10px 0">
       <el-pagination
           v-model:current-page="currentPage"
-          :page-size="10"
+          :page-size="pageSize"
           :page-sizes="[5,10,20]"
           layout="total, sizes, prev, pager, next, jumper"
           :total="total"
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
       />
+
     </div>
 
   </div>
@@ -63,6 +110,8 @@
 
 
 
+import request from "../utils/request";
+
 export default {
   name: 'Home',
   components: {
@@ -70,24 +119,45 @@ export default {
   },
   data() {
     return {
+      form:{},
+      dialogVisible: false,
       search: '',
       currentPage: 1,
-      total:10,
+      pageSize:10,
+      total:0,
       tableData :[
-        {
-          date: '2016-05-03',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        },
-        {
-          date: '2016-05-02',
-          name: 'Tom',
-          address: 'No. 189, Grove St, Los Angeles',
-        }
-      ]
+
+      ],
     }
   },
+  created() {
+    this.load()
+  },
   methods:{
+    load(){
+      request.get("/api/user",{
+        params:{
+          pageNum:this.currentPage,
+          pageSize:this.pageSize,
+          search:this.search
+        }
+
+      }).then(res =>{
+        console.log(res)
+        this.tableData = res.data.records
+        this.total = res.data.total
+
+      })
+    },
+    add(){
+      this.dialogVisible = true
+      this.form={}
+    },
+    save() {
+      request.post("/api/user",this.form).then(res =>{
+        console.log(res)
+      })
+    },
     handleEdit() {
 
     },
